b8470844e06a1a316b1f16d3b2b29f7c91d674a4
Mansidixit45 <mansidixit45@gmail.com>
2026-07-26 20:15:27 +0530

    fix overflow issue

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -9,7 +9,7 @@ const Layout = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
       <Navbar />
-      <main className={`flex-1 ${isHome ? "" : "w-full  px-4 sm:px-6 lg:px-8 py-6"}`}>
+      <main className={`flex-1 ${isHome ? " overflow-x-hidden" : "w-full  px-4 sm:px-6 lg:px-8 py-6"}`}>
         {children}
       </main>
       <Footer />
