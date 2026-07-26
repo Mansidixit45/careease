f8fbd69f8e8927966ff33b59bd34e1aa89bdc27e
Mansidixit45 <mansidixit45@gmail.com>
2026-07-26 21:34:24 +0530

    remove overflow hidden

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50 overflow-x-hidden">
+    <div className="min-h-screen flex flex-col bg-gray-50">
       <Navbar />
       <main className="flex-1 w-full">
         {children}
